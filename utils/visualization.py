6722f3e8b8b962646a99faa844f40fb8f01451b9
ashwinisugavi <ashwini.sagar1202@gmail.com>
2026-06-05 14:34:40 +0530

    Update visualization.py

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,1 +1,52 @@
+import plotly.express as px
 
+
+def line_chart(
+        df,
+        x_col,
+        y_col,
+        title
+):
+
+    fig = px.line(
+        df,
+        x=x_col,
+        y=y_col,
+        title=title
+    )
+
+    return fig
+
+
+def bar_chart(
+        df,
+        x_col,
+        y_col,
+        title
+):
+
+    fig = px.bar(
+        df,
+        x=x_col,
+        y=y_col,
+        title=title
+    )
+
+    return fig
+
+
+def scatter_chart(
+        df,
+        x_col,
+        y_col,
+        title
+):
+
+    fig = px.scatter(
+        df,
+        x=x_col,
+        y=y_col,
+        title=title
+    )
+
+    return fig
