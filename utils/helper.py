8151b1a8c9eaac538783e6b0ff54f32ade612502
ashwinisugavi <ashwini.sagar1202@gmail.com>
2026-06-05 14:34:59 +0530

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -1,1 +1,41 @@
+import joblib
+import pandas as pd
 
+
+def save_model(
+        model,
+        path
+):
+
+    joblib.dump(
+        model,
+        path
+    )
+
+
+def load_model(path):
+
+    return joblib.load(path)
+
+
+def create_prediction_dataframe(
+        values
+):
+
+    columns = [
+
+        "metric1",
+        "metric2",
+        "metric3",
+        "metric4",
+        "metric5",
+        "metric6",
+        "metric7",
+        "metric8",
+        "metric9"
+    ]
+
+    return pd.DataFrame(
+        [values],
+        columns=columns
+    )
