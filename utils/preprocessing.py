43bca900696a584308a198cd276e2792674dc6bb
ashwinisugavi <ashwini.sagar1202@gmail.com>
2026-06-05 14:03:47 +0530

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,1 +1,26 @@
+from sklearn.model_selection import train_test_split
 
+def split_data(df):
+
+    X = df[
+        [
+            'metric1',
+            'metric2',
+            'metric3',
+            'metric4',
+            'metric5',
+            'metric6',
+            'metric7',
+            'metric8',
+            'metric9'
+        ]
+    ]
+
+    y = df['failure']
+
+    return train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42
+    )
