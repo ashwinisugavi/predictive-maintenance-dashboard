662947603ccead1e30a75f92dc4947949a350391
ashwinisugavi <ashwini.sagar1202@gmail.com>
2026-06-05 14:06:16 +0530

    Update anomaly.py

--- utils/anomaly.py
+++ utils/anomaly.py
@@ -1,1 +1,63 @@
+import numpy as np
 
+
+def calculate_zscore(series):
+
+    return (
+        series - series.mean()
+    ) / series.std()
+
+
+def detect_anomaly(df, threshold=3):
+
+    temp_z = calculate_zscore(
+        df["metric1"]
+    )
+
+    vib_z = calculate_zscore(
+        df["metric7"]
+    )
+
+    df["Anomaly_Alert"] = np.where(
+
+        (abs(temp_z) > threshold)
+
+        |
+
+        (abs(vib_z) > threshold),
+
+        1,
+
+        0
+    )
+
+    return df
+
+
+def rolling_statistics(df):
+
+    df["temp_mean"] = (
+        df["metric1"]
+        .rolling(10)
+        .mean()
+    )
+
+    df["temp_std"] = (
+        df["metric1"]
+        .rolling(10)
+        .std()
+    )
+
+    df["vib_mean"] = (
+        df["metric7"]
+        .rolling(10)
+        .mean()
+    )
+
+    df["vib_std"] = (
+        df["metric7"]
+        .rolling(10)
+        .std()
+    )
+
+    return df
