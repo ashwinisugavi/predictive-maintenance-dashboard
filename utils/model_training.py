d421978911116ca6e307a456b27b8a3a6169d97e
ashwinisugavi <ashwini.sagar1202@gmail.com>
2026-06-05 14:04:09 +0530

    Update model_training.py

--- utils/model_training.py
+++ utils/model_training.py
@@ -1,1 +1,23 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 
+def get_models():
+
+    return {
+
+        "Linear Regression":
+        LinearRegression(),
+
+        "KNN":
+        KNeighborsRegressor(),
+
+        "Decision Tree":
+        DecisionTreeRegressor(),
+
+        "Random Forest":
+        RandomForestRegressor(
+            random_state=42
+        )
+    }
